97f46f307b447e797a298f23db00f68387f7fe18
siyinm <msy1827@126.com>
2020-04-18 12:32:34 +0800

    add 107

---
+++ 107-BinaryTreeLevelII/solution.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+           
+        if root == None: return []
+        bfs = [root]
+        ans = []
+        nex = []
+        
+        while bfs:
+            nex = []
+            temp = []
+            for node in bfs:
+                # bfs.pop(0)
+                temp.append(node.val)
+                if node.left!=None: 
+                    nex.append(node.left)
+                    
+                if node.right!=None: 
+                    nex.append(node.right)                
+            ans.append(temp)
+            bfs = nex
+            
+           
+        return ans[::-1]
