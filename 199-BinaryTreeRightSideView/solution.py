53d91518fa7bbd2fa7a285867239a93308cfd85a
siyinm <msy1827@126.com>
2020-04-22 15:23:27 +0800

    add 199

---
+++ 199-BinaryTreeRightSideView/solution.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if root == None: return []
+        bfs = [root]
+        ans = []
+        nex = []
+        
+        while bfs:
+            nex = []
+            temp = []
+            for node in bfs:
+                # bfs.pop(0)
+                temp.append(node.val)
+                if node.left != None: 
+                    nex.append(node.left)
+                    
+                if node.right != None: 
+                    nex.append(node.right)                
+            ans.append(temp[-1])
+            bfs = nex
+            
+           
+        return ans
